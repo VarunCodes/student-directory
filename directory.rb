830bd110b37ea0c9851e04a6ab71ff4699f5752b
Varun <mvarun@hotmail.co.uk>
2017-11-08 03:35:05 +0000

    Added cohort months as symbols

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 #Let's put all students in an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  ["Dr. Hannibal Lecter", :november],
+  ["Darth Vader", :november],
+  ["Nurse Ratched", :november],
+  ["Michael Corleone", :november],
+  ["Alex DeLarge", :november],
+  ["The Wicked Witch of the West", :november],
+  ["Terminator", :november],
+  ["Freddy Krueger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november]
 ]
 #and then print them
 def print_header
@@ -19,7 +19,7 @@ def print_header
 end
 def print(students)
     students.each do |student|
-    puts student
+        puts "#{student[0]} (#{student[1]} cohort)"
     end
 end
 def print_footer(students)
