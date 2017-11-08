49bd48022124636566e460c4eece2d60fd665ba5
Varun <mvarun@hotmail.co.uk>
2017-11-08 03:29:57 +0000

    Added header and footer

--- directory.rb
+++ directory.rb
@@ -13,10 +13,18 @@ students = [
   "Norman Bates"
 ]
 #and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
 end
-#finally we print the total number of students
-print "Overall, we have #{students.count} great students."
+def print(students)
+    students.each do |student|
+    puts student
+    end
+end
+def print_footer(students)
+    puts "Overall, we have #{students.count} great students."
+end
+print_header
+print(students)
+print_footer(students)
